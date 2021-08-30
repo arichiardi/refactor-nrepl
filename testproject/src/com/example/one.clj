3b6f4eacca6f5bb07937ed4bd5bf9cdc8d1a23ff
vemv <vemv@users.noreply.github.com>
2021-08-30 22:30:51 +0200

    Exercise `warn-on-reflection` in an additional place

--- testproject/src/com/example/one.clj
+++ testproject/src/com/example/one.clj
@@ -2,6 +2,9 @@
   (:require [com.example.two :as two :refer [foo]]
             [com.example.four :as four]))
 
+;; Tries reproducing https://github.com/clojure-emacs/clj-refactor.el/issues/485
+(set! *warn-on-reflection* true)
+
 (defn bar []
   (str "bar" (two/foo)))
 
